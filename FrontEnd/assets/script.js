95286df5160080c3b1d99f8586e2dfe7bcfb3f9d
pclmentc <pclmentc@gmail.com>
2024-01-08 16:35:08 +0100

    test affichage contenu works

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,6 +1,27 @@
 const apiCategories = 'http://localhost:5678/api/categories';
+const apiContenu = 'http://localhost:5678/api/works';
 
 fetch(apiCategories)
+    .then(response => {
+        // Vérification de la réussite de la requête (status 200 OK)
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP, statut : ${response.status}`);
+        }
+        // Conversion de la réponse en JSON
+        return response.json();
+    })
+    .then(data => {
+        // Affichage du résultat dans la console
+        console.log('Réponse de l\'API :', data);
+
+        // Vous pouvez également effectuer d'autres opérations avec les données ici
+    })
+    .catch(error => {
+        // Gestion des erreurs
+        console.error('Erreur lors de la requête :', error);
+    });
+
+fetch(apiContenu)
     .then(response => {
         // Vérification de la réussite de la requête (status 200 OK)
         if (!response.ok) {
