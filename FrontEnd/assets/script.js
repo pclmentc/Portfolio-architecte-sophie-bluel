882c326abad38bad0d88ea90d1a16646210c79af
pclmentc <pclmentc@gmail.com>
2024-01-08 14:55:23 +0100

    Amélioration de la mise en forme .Js

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,20 +1,21 @@
 const apiCategories = 'http://localhost:5678/api/categories';
-fetch(apiCategories)
-  .then(response => {
-    // Vérification de la réussite de la requête (status 200 OK)
-    if (!response.ok) {
-      throw new Error(`Erreur HTTP, statut : ${response.status}`);
-    }
-    // Conversion de la réponse en JSON
-    return response.json();
-  })
-  .then(data => {
-    // Affichage du résultat dans la console
-    console.log('Réponse de l\'API :', data);
 
-    // Vous pouvez également effectuer d'autres opérations avec les données ici
-  })
-  .catch(error => {
-    // Gestion des erreurs
-    console.error('Erreur lors de la requête :', error);
-  });
+fetch(apiCategories)
+    .then(response => {
+        // Vérification de la réussite de la requête (status 200 OK)
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP, statut : ${response.status}`);
+        }
+        // Conversion de la réponse en JSON
+        return response.json();
+    })
+    .then(data => {
+        // Affichage du résultat dans la console
+        console.log('Réponse de l\'API :', data);
+
+        // Vous pouvez également effectuer d'autres opérations avec les données ici
+    })
+    .catch(error => {
+        // Gestion des erreurs
+        console.error('Erreur lors de la requête :', error);
+    });
