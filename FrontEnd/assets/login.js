17d29f0847d6c23419be1a9ca5bfe0c5e4e69de6
pclmentc <pclmentc@gmail.com>
2024-01-15 15:14:45 +0100

    Correction erreur placement du fichier login.js

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -3,3 +3,5 @@
 // appeler le back avec une methode post en lui passant le payload
 // recuperer le token et le sauvegarder dans le localStorage ou le sessionStorage
 // PS : gerer les erreurs et empecher le changement de page natif du form
+
+
