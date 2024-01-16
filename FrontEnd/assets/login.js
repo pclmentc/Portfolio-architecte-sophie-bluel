819e5efc084c06a807e7a3b6ad44a53317a7f723
pclmentc <pclmentc@gmail.com>
2024-01-16 11:19:12 +0100

    récupération mot de passe et Email/ empécher le rechargement de la page

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,7 +1,17 @@
-// recuperer les entrees utilisateur (form : email et password)
+
 // verifier les entrees user
 // appeler le back avec une methode post en lui passant le payload
 // recuperer le token et le sauvegarder dans le localStorage ou le sessionStorage
+
+// recuperer les entrees utilisateur (form : email et password) 
+const form = document.querySelector('form');    
+    form.addEventListener("submit", (event) => {        
+        event.preventDefault();        
 // PS : gerer les erreurs et empecher le changement de page natif du form
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        console.log("Email:",email);
+        console.log("Mot de Passe:",password);
+});
 
 
